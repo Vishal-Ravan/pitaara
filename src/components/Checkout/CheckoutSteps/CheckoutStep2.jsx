493591fd806c96504aab36b4e6591038bd388cdb
Vishal-Ravan <vksingh2992000@gmail.com>
2025-04-22 20:21:04 +0530

    final changes go to live

--- src/components/Checkout/CheckoutSteps/CheckoutStep2.jsx
+++ src/components/Checkout/CheckoutSteps/CheckoutStep2.jsx
@@ -3,10 +3,14 @@ import { useRouter } from "next/router";
 
 // Generate or retrieve guest ID
 const getGuestId = () => {
-  const staticGuestId = "guest_static_12345";
-  localStorage.setItem("guestId", staticGuestId); // Always overwrite or ensure it's set
-  return staticGuestId;
+  let guestId = localStorage.getItem("guestId");
+  if (!guestId || !guestId.startsWith("guest_")) {
+    guestId = `guest_${Math.random().toString(36).substring(2, 15)}`;
+    localStorage.setItem("guestId", guestId);
+  }
+  return guestId;
 };
+
 
 
 // Check if the token is a guest ID
